f382aba85e70bfed22e25f6c288cdf5ed9ec23be
harsh2907 <harshv12.hv29@gmail.com>
2024-10-25 12:36:49 +0530

    fixed theme and status bar

--- app/src/main/java/com/example/trailtracker/MainActivity.kt
+++ app/src/main/java/com/example/trailtracker/MainActivity.kt
@@ -1,38 +1,26 @@
 package com.example.trailtracker
 
-import android.annotation.SuppressLint
-import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalView
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.compose.rememberNavController
 import com.example.trailtracker.mainScreen.services.TrackingService
+import com.example.trailtracker.navigation.Screens
 import com.example.trailtracker.navigation.TrailTrackerNavigation
-import com.example.trailtracker.onboarding.AuthenticationViewModel
 import com.example.trailtracker.ui.theme.TrailTrackerTheme
 import com.example.trailtracker.utils.Constants
 import com.example.trailtracker.utils.TrackingUtils
-import com.google.android.gms.location.LocationServices
-import com.google.android.gms.maps.model.LatLng
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.tasks.await
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -40,30 +28,19 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-
         setContent {
-            //TrailTracker
-            // - For those who love running off the beaten path and tracking their adventures.
             TrailTrackerTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
                     val navController = rememberNavController()
 
-                    val authenticationViewModel: AuthenticationViewModel = hiltViewModel()
-                    val startDestination by authenticationViewModel.startDestination.collectAsStateWithLifecycle()
-
-                    val window = (LocalView.current.context as Activity).window
-                    LaunchedEffect(Unit) {
-                        window.statusBarColor = Color.Black.toArgb()
-                    }
-
                     LaunchedEffect(intent) {
                         handleIntent(intent)
                     }
 
+                    val startDestination =  if (Firebase.auth.uid == null) Screens.AuthScreen.route else Screens.MainScreen.route
                     TrailTrackerNavigation(
                         navController = navController,
                         startDestination = startDestination
--- app/src/main/java/com/example/trailtracker/ui/theme/Theme.kt
+++ app/src/main/java/com/example/trailtracker/ui/theme/Theme.kt
@@ -1,17 +1,12 @@
 package com.example.trailtracker.ui.theme
 
 import android.app.Activity
-import android.os.Build
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
@@ -24,54 +19,22 @@ private val DarkColorScheme = darkColorScheme(
 
 )
 
-private val LightColorScheme = lightColorScheme(
-    primary = UiColors.primaryColor,
-    secondary = UiColors.secondaryColor,
-    primaryContainer = UiColors.primaryColor,
-    onPrimaryContainer = Color.Black,
-    onPrimary = Color.Black
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
-)
 
 @Composable
 fun TrailTrackerTheme(
-    //   darkTheme: Boolean = isSystemInDarkTheme(),
-    darkTheme: Boolean = true,
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-//        darkTheme -> DarkColorScheme
-//        else -> LightColorScheme
-        else -> DarkColorScheme
-    }
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            window.statusBarColor = UiColors.EerieBlack.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = false
         }
-
     }
 
     MaterialTheme(
-        colorScheme = colorScheme,
+        colorScheme = DarkColorScheme,
         typography = PoppinsTypography,
         content = content
     )
